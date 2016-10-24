7aa3b34870867580dc9d4672fee587f086165208
Milan Nikolic <milann17@live.com>
2016-10-24 10:58:21 +0200

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,18 @@
-angular.module("todoListApp", []);
+angular.module("todoListApp", ['ui.router'])
+.config(function($stateProvider, $urlRouterProvider){
+  $urlRouterProvider.otherwise('/');
+
+
+  $stateProvider
+        .state('home', {
+        url: '/',
+        templateUrl: '/',
+        controller: 'mainCtrl'
+      })
+
+        .state('about', {
+        url: '/about',
+        templateUrl: 'templates/two.html'
+        });
+    })
+
